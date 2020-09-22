3ebbd00a47c0644c622def0b17786d502ee2a84f
Priyo Setio <priyos.17@gmail.com>
2020-09-22 14:01:15 +0700

    Add files via upload

---
+++ 10-Operator Aritmatika/src/com/tutorial/Main.java
@@ -0,0 +1,43 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Operasi Aritmatika
+
+        int variable1 = 11;
+        int variable2 = 5;
+
+        int hasil;
+        // 1. Penjumlahan
+
+        hasil = variable1 + variable2;
+        System.out.println(variable1 + " + " + variable2 + " = " + hasil);
+
+        // 2. Pengurangan
+
+        hasil = variable1 - variable2;
+        System.out.printf("%d - %d = %d \n", variable1, variable2, hasil);
+
+        // 3. Perkalian
+
+        hasil = variable1 * variable2;
+        System.out.printf("%d x %d = %d \n", variable1, variable2, hasil);
+
+        // 4. Pembagian
+
+        hasil = variable1 / variable2;
+        System.out.printf("%d / %d = %d \n", variable1, variable2, hasil);
+
+        float a = 7;
+        float b = 2;
+        float hasilFloat = a/b;
+        System.out.println(a + " / " + b + " = " + hasilFloat);
+
+        // 5. Modulus
+        hasil = variable1 % variable2;
+        System.out.printf("%d %% %d = %d \n", variable1, variable2, hasil);
+
+    }
+}
