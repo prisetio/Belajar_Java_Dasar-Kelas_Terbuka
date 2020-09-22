a52031a9f822a15bd77c053a196db54f831c2f71
Priyo Setio <priyos.17@gmail.com>
2020-09-22 07:08:08 +0700

    Add files via upload

---
+++ 08-Variable/src/com/tutorial/Main.java
@@ -0,0 +1,23 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // kita akan membuat variable
+        int x = 10; //assignment
+        System.out.println("Nilai x = " + x);
+
+        x = 20;
+        System.out.println("Nilai x baru = " + x);
+    /* dalam penamaan di javatidak boleh menggunakan nama
+yang sudah dipakai oleh java */
+
+        // kita akan membuat sebuah deklarasi
+        int b; // deklarasi jika b adalah integer
+        b = 7;
+        System.out.println("Nilai b = " + b);
+    }
+}
+
+
