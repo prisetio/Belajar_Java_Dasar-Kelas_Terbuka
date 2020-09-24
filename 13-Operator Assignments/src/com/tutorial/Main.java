a231935d2ebe0334b4fc5306dc37018b85f68ae3
Priyo Setio <priyos.17@gmail.com>
2020-09-24 13:13:59 +0700

    Add files via upload

---
+++ 13-Operator Assignments/src/com/tutorial/Main.java
@@ -0,0 +1,33 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // operator assignment
+
+        // assignment operator penjumlahan
+        int a = 1;
+        a += 5; // a = a + 5
+        System.out.println("nilai a = " +a);
+
+        // assignment operator pengurangan
+        int b = 7;
+        b -= 2;
+        System.out.println("nilai b = " +b);
+
+        // assignment operator perkalian
+        int c = 7;
+        c *= 2;
+        System.out.println("nilai c = " +c);
+
+        // assignment operator pembagian
+        int d = 7;
+        d /= 2;
+        System.out.println("nilai d = " +d);
+
+        // assignment operator modulus
+        int e = 7;
+        e %= 2;
+        System.out.println("nilai e = " +e);
+    }
+}
